a164ade405011ff4d820a07d35f2d92de7358a3c
kiuchiyoji <o1103011@gm.pu.edu.tw>
2023-03-22 13:52:35 +0800

    V.1.1(木内陽士) 修改文字

--- app/src/main/java/tw/edu/pu/o1103011/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1103011/MainActivity.kt
@@ -2,10 +2,19 @@ package tw.edu.pu.o1103011
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
+lateinit var txv: TextView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv=findViewById(R.id.txv)
+        if (txv.text =="木内您好！"){
+          txv.text  =="海青班很棒"
+        }
+        else{
+            txv.text ="木内您好！"
+        }
     }
 }
