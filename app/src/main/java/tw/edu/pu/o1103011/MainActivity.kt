9063db4235f4bac6534b7f502e86cdf590a643fb
kiuchiyoji <o1103011@gm.pu.edu.tw>
2023-03-22 14:52:43 +0800

    V.1.2(木内陽士)

--- app/src/main/java/tw/edu/pu/o1103011/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/o1103011/MainActivity.kt
@@ -2,6 +2,7 @@ package tw.edu.pu.o1103011
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.TextView
 
 lateinit var txv: TextView
@@ -9,12 +10,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        txv=findViewById(R.id.txv)
-        if (txv.text =="木内您好！"){
-          txv.text  =="海青班很棒"
-        }
-        else{
-            txv.text ="木内您好！"
-        }
+        txv = findViewById(R.id.txv)
+        txv.setOnClickListener({
+            if (txv.text == "木内您好!"){
+                txv.text = "海青班很棒,數媒更是好的方向"
+            }
+            else{
+                txv.text = "木内您好!"
+            }
+        })
     }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        var txv:TextView = findViewById(R.id.txv)
+        txv.text = "螢幕觸控"
+        return true
+    }
+
+
 }
+
+
